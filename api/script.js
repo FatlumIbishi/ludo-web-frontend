2a50fa5c0289677545a2cf9d4d112a2772b7360b
FatlumIbishi <fatlum.ibishi@gmail.com>
2019-01-24 16:02:15 +0100

    Colors

--- api/script.js
+++ api/script.js
@@ -1,24 +1,41 @@
 const board = document.getElementById('board');
 
-document.getElementById('joinGame').onclick = function() {
-    
-    var request = new XMLHttpRequest();
-    var requestURL = 'https://ghibliapi.herokuapp.com/films'
-    request.open('GET', requestURL, true);
-    
-    request.onload = function () {
+document.getElementById('joinGame').onclick = function () {
 
-        var data = JSON.parse(this.response);
-        
-        data.forEach(movie => {
-            const p = document.createElement('p');
-            if (movie.rt_score == document.getElementById('gamecode').value) {
-                p.textContent = movie.title
+    str = document.getElementById("gamecode").value
+    const p = document.createElement('p');
+
+    if (str.length == 0) {
+        p.textContent = "Nothing typed!";
+        board.appendChild(p);
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+
+            data = JSON.parse(this.responseText);
+
+            if (this.readyState == 4 && this.status == 200) {
+                p.textContent = this.responseText;
+                board.appendChild(p);
+            } else {
+                p.textContent = this.status;
                 board.appendChild(p);
             }
-        });
+        };
+        xmlhttp.open("GET", "https://localhost:5001/api/ludo/" + str + "/getgamedetails/", true);
+        xmlhttp.send();
     }
-    request.send();
+
 }
 
+document.getElementById('newGame').onclick = function () {
 
+    var request = new XMLHttpRequest();
+    var requestURL = 'https://localhost:5001/api/ludo/createnewgame'
+    request.open('POST', requestURL, true);
+
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    request.send();
+}
